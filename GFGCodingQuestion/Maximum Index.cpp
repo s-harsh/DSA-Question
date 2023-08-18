1efdac285aa7361c56ed48d51d9137a2ebf0ee2e
Harsh Soni <77559862+s-harsh@users.noreply.github.com>
2023-08-19 03:28:10 +0530

    Update Maximum Index

--- GFGCodingQuestion/Maximum Index.cpp
+++ GFGCodingQuestion/Maximum Index.cpp
@@ -15,3 +15,22 @@ class Solution{
         return ans;
     }
 };
+
+// Little more optimize 
+// just make i is less then from j
+class Solution{
+    public:
+    // Function to find the maximum index difference.
+    int maxIndexDiff(int arr[], int n) 
+    { 
+        int ans=0;
+        for(int j=0;j<n;j++){
+            for(int i=0;i<j;i++){
+                if(arr[i]<=arr[j] && ans<(j-i)){
+                    ans=j-i;
+                }
+            }
+        }
+        return ans;
+    }
+};
