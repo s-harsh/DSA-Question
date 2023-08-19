aca44c14b09c9afc243ed42b18e9f2a5cde9b733
Harsh Soni <77559862+s-harsh@users.noreply.github.com>
2023-08-20 02:41:25 +0530

    Update First negative integer in every window of size k

--- GFGCodingQuestion/First negative integer in every window of size k.cpp
+++ GFGCodingQuestion/First negative integer in every window of size k.cpp
@@ -23,3 +23,33 @@ vector<long long> printFirstNegativeInteger(long long int a[],
         return ans;
                                                  
  }
+
+// Sliding Window 
+vector<long long> printFirstNegativeInteger(long long int a[],
+                                             long long int n, long long int k) {
+        long long i=0,j=0;
+        vector<long long>ans;
+        queue<int>q;
+        while(j<n){
+            if(a[j]<0){
+                q.push(a[j]);
+            }
+            if(j-i+1<k){
+                j++;
+            }
+            else if(j-i+1==k){
+                if(q.empty()){
+                    ans.push_back(0);
+                }
+                else{
+                    ans.push_back(q.front());
+                    if(a[i]<0){
+                        q.pop();
+                    }
+                }
+                i++,j++;
+            }
+        }
+        return ans;
+                                                 
+ }
