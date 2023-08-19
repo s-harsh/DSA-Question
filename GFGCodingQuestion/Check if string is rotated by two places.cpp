76e41b5065faa706187937c79d1e72e3e189013d
Harsh Soni <77559862+s-harsh@users.noreply.github.com>
2023-08-19 17:00:28 +0530

    Update Check if string is rotated by two places

--- GFGCodingQuestion/Check if string is rotated by two places.cpp
+++ GFGCodingQuestion/Check if string is rotated by two places.cpp
@@ -20,3 +20,27 @@ public:
         return false;
     }
 };
+
+// Another Approach 
+class Solution
+{
+public:
+    // Function to check if a string can be obtained by rotating
+    // another string by exactly 2 places.
+    bool isRotated(string str1, string str2)
+    {
+        if (str1.size() != str2.size()) {
+            return false;
+        }
+        if(str1.size()==1){
+            return str1==str2;
+        }
+        int n=str1.size();
+        string left="",right="";
+        for(int i=0;i<n;i++){
+            left.push_back(str1[(i+2)%n]);
+            right.push_back(str1[(n-2+i)%n]);
+        }
+        return (left==str2) || (right==str2);
+    }
+};
