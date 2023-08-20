91910361403fe15f0a19427d226e8211f94ee9c7
Harsh Soni <77559862+s-harsh@users.noreply.github.com>
2023-08-21 01:36:59 +0530

    Update Longest K unique characters substring

--- GFGCodingQuestion/Longest K unique characters substring.cpp
+++ GFGCodingQuestion/Longest K unique characters substring.cpp
@@ -26,3 +26,27 @@ class Solution{
         return ans;
     }
 };
+
+// Little Optimize way
+class Solution{
+  public:
+    int longestKSubstr(string s, int k) {
+        int i=0,j=0,ans=-1,n=s.size();
+        unordered_map<char,int>mp;
+        while(j<n){
+            mp[s[j]]++;
+            while(mp.size()>k){
+                mp[s[i]]--;
+                if(mp[s[i]]==0){
+                    mp.erase(s[i]);
+                }
+                i++;
+            }
+            if(mp.size()==k){
+                ans=max(ans,(j-i+1));
+            }
+            j++;
+        }
+        return ans;
+    }
+};
