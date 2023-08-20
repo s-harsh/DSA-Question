43c3ef12a5a575f51c2395c444ff9bbba93144d8
Harsh Soni <77559862+s-harsh@users.noreply.github.com>
2023-08-21 01:32:16 +0530

    Create Longest K unique characters substring
    Variable size Sliding Window problem

---
+++ GFGCodingQuestion/Longest K unique characters substring.cpp
@@ -0,0 +1,28 @@
+class Solution{
+  public:
+    int longestKSubstr(string s, int k) {
+        int i=0,j=0,ans=-1,n=s.size();
+        unordered_map<char,int>mp;
+        while(j<n){
+            mp[s[j]]++;
+            if(mp.size()<k){
+                j++;
+            }
+            else if(mp.size()==k){
+                ans=max(ans,(j-i+1));
+                j++;
+            }
+            else if(mp.size()>k){
+                while(mp.size()>k){
+                    mp[s[i]]--;
+                    if(mp[s[i]]==0){
+                        mp.erase(s[i]);
+                    }
+                    i++;
+                }
+                j++;
+            }
+        }
+        return ans;
+    }
+};
