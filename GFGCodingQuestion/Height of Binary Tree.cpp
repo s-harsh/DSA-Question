16c0f8343bdde47e9d521d69d246bea857b8481a
Harsh Soni <77559862+s-harsh@users.noreply.github.com>
2023-08-21 19:00:16 +0530

    Create Height of Binary Tree

---
+++ GFGCodingQuestion/Height of Binary Tree.cpp
@@ -0,0 +1,13 @@
+// Using recursion 
+class Solution{
+    public:
+    //Function to find the height of a binary tree.
+    int height(struct Node* node){
+        if(node==NULL){
+            return 0;
+        }
+        int l=height(node->left);
+        int r=height(node->right);
+        return 1+max(l,r);
+    }
+};
